d50aa3ab4e60a8e304dd3e4df6bf85a9fa5aff9d
codeamac08 <codeamac08@gmail.com>
2015-10-27 18:22:20 -0600

    Ejercicio para entender el uso de self

---
+++ self_keyword.rb
@@ -0,0 +1,27 @@
+puts "self es igual a: #{self}"
+
+class DummyClass
+  puts "Esto esta corriendo cuando se define la clase"
+  puts "En este contexto self es igual a: #{self}"
+end
+
+class DummyClass
+
+	def instance_method 
+	puts "Dentro de un método instancia"
+	puts "En este contexto self es igual a: #{self}"
+	end
+end
+dummy_class = DummyClass.new()
+puts dummy_class.instance_method
+
+class DummyClass 
+
+	def self.class_method
+		puts "Dentro de un método de clase"
+		puts "En este contexto self es igual a: #{self}"
+	end
+end
+
+dummy_class = DummyClass.new()
+puts DummyClass.class_method
